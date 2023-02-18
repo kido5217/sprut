ce6def109d4543576645536b1a17025f9ceb7bb6
Fedor Suchkov <f.suchkov@gmail.com>
2023-02-18 15:09:34 +0000

    Make `sptur.database` functions async. Add database initialisation function that creates missing collections.

--- sprut/database/database.py
+++ sprut/database/database.py
@@ -2,13 +2,28 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from sprut.settings.database import mongodb_settings
 
+COLLECTIONS: tuple[str, ...] = ("devices",)
 
-def get_database(
+
+async def init_database(database: AsyncIOMotorDatabase) -> None:
+    existing_collections: list[str] = await database.list_collection_names()  # type: ignore
+    commections_to_create: list[str] = [
+        collection
+        for collection in COLLECTIONS
+        if collection not in existing_collections
+    ]
+    for collection in commections_to_create:
+        await database.create_collection(collection)  # type: ignore
+
+
+async def get_database(
     url: str = mongodb_settings.url, db_name: str = mongodb_settings.db_name
 ) -> AsyncIOMotorDatabase:
     """Create instance of database object."""
 
     client: AsyncIOMotorClient = AsyncIOMotorClient(url)
     database: AsyncIOMotorDatabase = client[db_name]
+
+    await init_database(database)
 
     return database
--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -4,10 +4,11 @@ from pytest import mark
 from sprut.database.database import get_database
 
 
-def test_database_client_creation() -> None:
+@mark.asyncio
+async def test_database_client_creation() -> None:
     """Check that database connection object is created successfully."""
 
-    database: AsyncIOMotorDatabase = get_database()
+    database: AsyncIOMotorDatabase = await get_database()
     assert isinstance(database, AsyncIOMotorDatabase)
 
 
@@ -15,7 +16,7 @@ def test_database_client_creation() -> None:
 async def test_database_client_connection() -> None:
     """Check that client can connect to test database."""
 
-    database: AsyncIOMotorDatabase = get_database()
+    database: AsyncIOMotorDatabase = await get_database()
     # TODO: open PR in motor-types
     ping: dict[str, float] = await database.command({"ping": 1})  # type: ignore
     assert ping == {"ok": 1.0}
